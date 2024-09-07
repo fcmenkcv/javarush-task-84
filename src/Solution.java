11e1f29e52abaf1573504cfa3977c8c0895da8b1
fcmenkcv <fcmenkcv@gmail.com>
2024-09-07 18:53:09 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task06.task0608;
+
+/*
+Кубический калькулятор
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    // напишите тут ваш код
+    public static long cube(long num) {
+        return (num * num * num);
+    }
+}
